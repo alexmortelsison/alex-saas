5a2bd38f6fce92dc5ed8c9578745c7e892062c5d
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-10-20 19:48:28 +0900

    initial commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import React from "react";
+import { ThemeToggle } from "./ThemeToggle";
+import { Button } from "@/components/ui/button";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+export default async function Navbar() {
+  const { isAuthenticated } = getKindeServerSession();
+  return (
+    <nav className="border-b bg-background h-[10vh] flex items-center justify-center">
+      <div className="container flex items-center justify-between">
+        <Link href="/">
+          <h1 className="font-bold text-3xl">
+            Alex<span className="text-primary">Saas</span>
+          </h1>
+        </Link>
+
+        <div className="flex items-center gap-x-5">
+          <ThemeToggle />
+
+          {(await isAuthenticated()) ? (
+            <LogoutLink>
+              <Button>Log out</Button>
+            </LogoutLink>
+          ) : (
+            <div className="flex items-center gap-x-5">
+              <LoginLink>
+                <Button>Sign In</Button>
+              </LoginLink>
+
+              <RegisterLink>
+                <Button variant="secondary">Sign Up</Button>
+              </RegisterLink>
+            </div>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+}
